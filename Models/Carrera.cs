f1dfbcfa9906f4e14e5deb626b6eaf2f0cfc7cdb
diego.correa <diego.correa.cumbicus07@outlook.com>
2024-05-02 07:52:44 -0500

    Creación de Modelo DCorrea y  relación 1 a 1 con un nuevo objeto llamado Carrera

---
+++ Models/Carrera.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiegoCorrea.Models
+{
+    public class Carrera
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string NombreCarrera { get; set; }
+
+        [AllowedValues]
+        public string Campus { get; set; }
+
+        public int NumeroSemestres { get; set; }
+
+        public DCorrea DCorrea { get; set; }
+
+    }
+}
---
+++ Models/DCorrea.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DiegoCorrea.Models
+{
+    public class DCorrea
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        public int NumHijos { get; set; }
+
+        [AllowedValues]
+        public decimal Salario { get; set; }
+
+        public string Ciudad { get; set; }
+
+        [Required]
+        public bool Activo { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public int CarreraId { get; set; }
+
+        [ForeignKey("CarreraId")]
+        public Carrera? Carrera { get; set; }
+
+    }
+}
